01e35534b399766daf02143e5dbeeed78d3ad348
artemchikkey <160394124+artemchikkey@users.noreply.github.com>
2024-04-30 18:59:41 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,34 +1,41 @@
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+
 #include <string>
+
 template<typename T, int size>
 class TStack {
  private:
-    static const int SSize = size;
-    T data[SSize];
+    static const int KSize = size;
+    T data[KSize];
     int top;
+
  public:
     TStack() : top(0) {}
+
     void push(const T &item) {
         if (top <= size - 1)
             data[top++] = item;
         else
-            throw std::string("Full slots");
+            throw std::string("Full!");
     }
+
     T pop() {
         if (top > 0)
             return data[--top];
         else
-            throw std::string("Empty slots");
+            throw std::string("Empty!");
     }
-    bool isEmpty()
+
+    bool isEmpty() {
         return top == 0;
     }
+
     T get() {
         if (!isEmpty())
             return data[top - 1];
         else
-            throw std::string("Empty!");
+            throw std::string("Empty");
     }
 };
 #endif  // INCLUDE_TSTACK_H_
